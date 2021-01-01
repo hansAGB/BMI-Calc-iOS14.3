f43efb7298aa377d99589cfda69910a28a742de5
hansAGB <hans.aangeenbrug@icloud.com>
2021-01-01 17:12:52 +0100

    Initial Commit

---
+++ BMI Calc iOS14.3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI Calc iOS14.3
+//
+//  Created by Hans Aangeenbrug on 01/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
