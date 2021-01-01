ace4b65b4060745aaf7ad7c748b33874b3a825ba
hansAGB <hans.aangeenbrug@icloud.com>
2021-01-01 22:08:04 +0100

    BMI calculated now get it to net view

---
+++ BMI Calc iOS14.3/Controllers/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  BMI Calc iOS14.3
+//
+//  Created by Hans Aangeenbrug on 01/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        heightSlider.value = 1.5
+        weightSlider.value = 100
+    }
+
+    @IBAction func heightSliderChanged(_ sender:
+        UISlider) {
+        heightLabel.text = "\(String(format: "%.2f", sender.value)) m"
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        weightLabel.text = "\(Int(sender.value)) kg"
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        var bmi: Float = 0
+        
+        bmi = weight / (height * height)
+        
+        print(String(format: "%.1f", bmi))
+        print(bmi)
+
+    }
+}
+
